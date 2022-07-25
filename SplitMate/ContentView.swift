53a261463061e3e626aa5b0494c23ea4a5144909
gibert-j <adelgibert@gmail.com>
2022-07-26 01:19:01 +1200

    Added bool to track if textfield is on focus Added "Done" button to decimal keyboard so it can be hidden

--- SplitMate/ContentView.swift
+++ SplitMate/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
     let tipPercentages = [10, 15, 20, 25, 0]
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
@@ -29,12 +31,13 @@ struct ContentView: View {
             
             Form {
                 Section {
-                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "NZD"))
-                            .keyboardType(.decimalPad)
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "NZD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
-                    Picker("Number of peeps?", selection: $numberOfPeople) {
+                    Picker("Number of people?", selection: $numberOfPeople) {
                         ForEach(2..<100) {
-                            Text("\($0) peeps")
+                            Text("\($0) people")
                         }
                     }
                 }
@@ -48,11 +51,22 @@ struct ContentView: View {
                 } header: {
                     Text("Tip % to leave?")
                 }
+                
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "NZD"))
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationTitle("SplitMate")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
