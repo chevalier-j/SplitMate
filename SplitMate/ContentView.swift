a3713bfa70d061c81fca362855918e5413c5f6cb
gibert-j <adelgibert@gmail.com>
2022-07-25 23:34:05 +1200

    Added tip picker with segmented control and header to it. Added logic to calculate the bill split per person.

--- SplitMate/ContentView.swift
+++ SplitMate/ContentView.swift
@@ -13,6 +13,16 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
@@ -29,13 +39,26 @@ struct ContentView: View {
                     }
                 }
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "NZD"))
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Tip % to leave?")
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "NZD"))
                 }
             }
             .navigationTitle("SplitMate")
         }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
