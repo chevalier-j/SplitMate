8cd6895531470777034734ee63f33b77b7a0d678
gibert-j <adelgibert@gmail.com>
2022-07-25 22:01:24 +1200

    Initial variables declared

--- SplitMate/ContentView.swift
+++ SplitMate/ContentView.swift
@@ -1,16 +1,25 @@
 //
 //  ContentView.swift
-//  SplitMate
+//  WeSplitApp
 //
-//  Created by GibertJ on 25/07/22.
+//  Created by GibertJ on 23/07/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "NZD"))
+            }
+        }
     }
 }
 
