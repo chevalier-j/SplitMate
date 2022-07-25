1bf863c3023b9bdf6f65cf6e29d1f634d929cd16
gibert-j <adelgibert@gmail.com>
2022-07-25 21:59:42 +1200

    Initial Commit

---
+++ SplitMate/SplitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitMateApp.swift
+//  SplitMate
+//
+//  Created by GibertJ on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
